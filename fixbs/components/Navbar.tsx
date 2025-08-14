d77283e95ceb844b03f18c2d197c9893da259da7
EbadullahSiddiqi <official.ebadullahsiddiqi@gmail.com>
2025-08-14 17:26:00 +0500

    Improved the Navbar's UX a little

--- fixbs/components/Navbar.tsx
+++ fixbs/components/Navbar.tsx
@@ -7,13 +7,22 @@ function Navbar() {
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-white text-2xl font-bold">FixBS</h1>
         <div className="flex gap-3 space-x-4 text-white text-lg font-semibold">
-          <Link className="hover:text-[#FFEC9F]" href="/">
+          <Link
+            className="hover:text-[#FFEC9F] transition-all duration-200"
+            href="/"
+          >
             Home
           </Link>
-          <Link className="hover:text-[#FFEC9F]" href="/about">
+          <Link
+            className="hover:text-[#FFEC9F] transition-all duration-200"
+            href="/about"
+          >
             About
           </Link>
-          <Link className="hover:text-[#FFEC9F]" href="/contact">
+          <Link
+            className="hover:text-[#FFEC9F] transition-all duration-200"
+            href="/contact"
+          >
             Contact
           </Link>
         </div>
